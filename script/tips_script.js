f81f7ddcdad487f3b2dacc9ac5f39ef29cb2cf0a
Jesús Quiñones <1930318@upv.edu.mx>
2023-04-24 12:06:59 -0600

    adding the actual project status

---
+++ script/tips_script.js
@@ -0,0 +1,7 @@
+// Functions fot he tips program
+
+var billAmount = document.getElementById("billAmount").value;
+var serviceQuality = document.getElementById("serviceQuality").value;
+var numPeople = document.getElementById("numPeople").value;
+
+
